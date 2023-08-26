7711edadedd679fee178210ef8ef12fb5ee10032
Deniz <48675506+denizk0461@users.noreply.github.com>
2023-08-26 19:48:11 +0200

    Added game version 0.0.1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "denizk0461-Swords _ Stuff-0.0.1";
+const contentToCache = [
+    "Build/0.0.1.loader.js",
+    "Build/0.0.1.framework.js.br",
+    "Build/0.0.1.data.br",
+    "Build/0.0.1.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
